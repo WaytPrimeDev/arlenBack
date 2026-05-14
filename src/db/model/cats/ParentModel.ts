aaa72095ac5d5e6980ac019cb50051c161b64f66
WaytPrimeDev <206768277+WaytPrimeDev@users.noreply.github.com>
2026-05-14 21:15:28 +0300

    add news

--- src/db/model/cats/ParentModel.ts
+++ src/db/model/cats/ParentModel.ts
@@ -11,29 +11,35 @@ const imageSchema = new Schema(
   { _id: false },
 );
 
-const ParentSchema = new Schema({
-  nameUa: { type: String, required: true },
-  nameEn: { type: String, required: true },
-  color: { type: String, required: true },
-  familyId: [{ type: Schema.Types.ObjectId, ref: "Family", default: null }],
-  // title: { type: String, required: true },
-  breed: { type: String, required: true },
-  // userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  sex: { type: String, enum: Object.values(Sex), required: true },
-  Kittens: [{ type: Schema.Types.ObjectId, ref: "Kitten", default: null }],
-  images: {
-    type: [imageSchema],
-    validate: [
-      {
-        validator: (arr: any[]) => arr.length >= 1,
-        message: "At least 1 image is required",
-      },
-      {
-        validator: (arr: any[]) => arr.length <= 5,
-        message: "Max 5 images allowed",
-      },
-    ],
+const ParentSchema = new Schema(
+  {
+    nameUa: { type: String, required: true },
+    nameEn: { type: String, required: true },
+    color: { type: String, required: true },
+    familyId: [{ type: Schema.Types.ObjectId, ref: "Family", default: null }],
+    // title: { type: String, required: true },
+    breed: { type: String, required: true },
+    // userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    sex: { type: String, enum: Object.values(Sex), required: true },
+    Kittens: [{ type: Schema.Types.ObjectId, ref: "Kitten", default: null }],
+    images: {
+      type: [imageSchema],
+      validate: [
+        {
+          validator: (arr: any[]) => arr.length >= 1,
+          message: "At least 1 image is required",
+        },
+        {
+          validator: (arr: any[]) => arr.length <= 5,
+          message: "Max 5 images allowed",
+        },
+      ],
+    },
   },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 export const ParentModel = model("Parent", ParentSchema);
